0457f7ddf589122dac58aeb83d4bcbc71ee58e02
Omik Futerkowski <dawidszab@wp.pl>
2020-11-18 22:11:16 +0100

    Added 2 test cases with boundle directory and readme

---
+++ Gral-boundle/Chrome/gral-wyszukaj-producent.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+import logging
+
+logger = logging.getLogger('Lab2 TAU')
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+driver = webdriver.Chrome(executable_path='C:\Program Files (x86)\Google\Chrome\chromedriver.exe')
+
+driver.get('http://www.gral.pl/')
+logger.info("Opening http://www.gral.pl/...")
+
+search = driver.find_element_by_id("szukaj01")
+search.click()
+search.send_keys("Spartan")
+logger.info("Searching for 'Spartan'...")
+
+try:
+    driver.find_element_by_xpath("/html/body/table[3]/tbody/tr[1]/td[1]/form/table/tbody/tr/td[3]/select/option[114]").click()
+    logger.info("Searching for manufacturer 'SilentiumPC'...")
+except:
+    logger.error("Unable to find desired element")
+
+driver.find_element_by_name("bks33222").click()
+logger.info("Initiate searching...")
+logger.info("Checking if search results contain desired items...")
+try:
+    driver.find_element_by_partial_link_text('Spartan')
+except:
+    logger.critical("Unable to find link to: http://www.gral.pl/SilentiumPC+Spartan+3+LT+HE1012_179.html")
+else:
+    logger.info("Test was completed successfuly")
+
+driver.close()
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# TAU_Java
+###Testowana strona: Gral.pl###
+
+#Scenariusz 1 - plik "gral-dodaj-koszyk-chrome.py"
+
+Wchodzimy na stronê gral.pl
+Wchodzimy w zak³adkê "obudowy"
+Dodajemy do koszyka obudowê "Modecom Oberon Pro LE USB 3.0"
+Wchodzimy ponownie w zak³adkê "obudowy"
+Ponownie dodajemy do koszyka obudowê "Modecom Oberon Pro LE USB 3.0"
+Sprawdzamy czy w koszyku znajduj¹ siê 2 identyczne obudowy
+
+#Scenariusz 2 - plik "gral-wyszukaj-producent.py"
+
+Wchodzimy na stronê gral.pl
+W polu wyszukaj wpisujemy "Spartan"
+W drop liœcie wybieramy producenta "SilentiumPC"
+Wyszukujemy u¿ywaj¹c czerwonego buttona
+Sprawdzamy czy po wyszukaniu pojawi³y siê elementy z linkami do zakupu produktu
