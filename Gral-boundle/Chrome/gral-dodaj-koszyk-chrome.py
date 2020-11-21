c2e28c42c52d6fd6dbf0420f4900035c24fafee8
Omik Futerkowski <dawidszab@wp.pl>
2020-11-21 18:52:20 +0100

    Minor changes

--- Gral-boundle/Chrome/gral-dodaj-koszyk-chrome.py
+++ Gral-boundle/Chrome/gral-dodaj-koszyk-chrome.py
@@ -1,48 +1,50 @@
 from selenium import webdriver
 import logging
 
-logger = logging.getLogger('Lab2 TAU')
-logger.setLevel(logging.INFO)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
 
-driver = webdriver.Chrome(executable_path='C:\Program Files (x86)\Google\Chrome\chromedriver.exe')
+class AddCart:
+    logger = logging.getLogger('Lab2 TAU')
+    logger.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
 
-driver.get('http://www.gral.pl/')
-logger.info("Opening http://www.gral.pl/...")
+    driver = webdriver.Chrome(executable_path='C:\Program Files (x86)\Google\Chrome\chromedriver.exe')
 
-temp = driver.find_element_by_id("identmenu122")
-temp.click()
-logger.info("Opening tab 'Obudowy'...")
+    driver.get('http://www.gral.pl/')
+    logger.info("Opening http://www.gral.pl/...")
 
-xpath = "/html/body/table[5]/tbody/tr/td[2]/table[2]/tbody/tr[1]/td[1]/table/tbody/tr/td[1]/table/tbody/tr[5]/td/table/tbody/tr/td[2]/div/form/button"
-try:
-    driver.find_element_by_xpath(xpath).click()
-    logger.info("Adding to cart: 'Modecom Oberon Pro LE USB 3.0'...")
-except:
-    logger.warning("Problem with getting element: 'add to cart button'")
+    temp = driver.find_element_by_id("identmenu122")
+    temp.click()
+    logger.info("Opening tab 'Obudowy'...")
 
-temp = driver.find_element_by_id("identmenu122")
-temp.click()
-logger.info("Opening tab 'Obudowy'...")
+    xpath = "/html/body/table[5]/tbody/tr/td[2]/table[2]/tbody/tr[1]/td[1]/table/tbody/tr/td[1]/table/tbody/tr[5]/td/table/tbody/tr/td[2]/div/form/button"
+    try:
+        driver.find_element_by_xpath(xpath).click()
+        logger.info("Adding to cart: 'Modecom Oberon Pro LE USB 3.0'...")
+    except:
+        logger.warning("Problem with getting element: 'add to cart button'")
 
-try:
-    driver.find_element_by_xpath(xpath).click()
-    logger.info("Adding to cart: 'Modecom Oberon Pro LE USB 3.0'...")
-except:
-    logger.warning("Problem with getting element: 'add to cart button'")
+    temp = driver.find_element_by_id("identmenu122")
+    temp.click()
+    logger.info("Opening tab 'Obudowy'...")
 
-temp = driver.find_element_by_id("ilek1")
-orderCount = temp.get_attribute("value")
+    try:
+        driver.find_element_by_xpath(xpath).click()
+        logger.info("Adding to cart: 'Modecom Oberon Pro LE USB 3.0'...")
+    except:
+        logger.warning("Problem with getting element: 'add to cart button'")
 
-logger.info('Checking if number of items in cart is correct...')
+    temp = driver.find_element_by_id("ilek1")
+    orderCount = temp.get_attribute("value")
 
-if int(orderCount) == 2:
-    logger.info('Test was completed successfuly')
-else:
-    logger.error('Expected value = 2 instead got: ', int(orderCount))
+    logger.info('Checking if number of items in cart is correct...')
 
-driver.close()
+    if int(orderCount) == 2:
+        logger.info('Test was completed successfuly')
+    else:
+        logger.error('Expected value = 2 instead got: ', int(orderCount))
+
+    driver.close()
